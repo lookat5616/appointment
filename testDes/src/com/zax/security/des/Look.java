bcf4fbb92d9e9f6cbca3e80a6d8d67c8deb57af3
look <look@ZXW-20160413WGR>
2016-11-16 17:45:53 +0800

    发的发的是

---
+++ testDes/src/com/zax/security/des/Look.java
@@ -0,0 +1,23 @@
+package com.zax.security.des;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Look {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList();
+		list.add("11");
+		list.add("12");
+		list.add("13");
+		list.add("14");
+		list.add("15");
+		list.add("15");
+		list.add("15");
+		list.add("15");
+        for (String str : list) {
+			System.out.println(str);
+		}
+		
+	}
+
+}
